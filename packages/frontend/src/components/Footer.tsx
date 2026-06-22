82382eb17cd0ca29acbbcc4ff24b867cf77d897e
Aiden-Cherniske <Aiden.Cherniske@atln.com>
2026-06-22 10:26:43 -0400

    style: Update to more reasonable footer. TODO make this a toggle option?

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -7,17 +7,8 @@ const Footer = () => {
     <footer className="w-full bg-bg text-text mt-auto">
       <div className="mx-auto max-w-7xl px-6 py-12 sm:py-14">
         <div className="flex flex-col items-center justify-center gap-5 text-center">
-          <div className="w-full flex justify-center">
-            {/* Wordmark Asset: Reactive to system dark preference preferences */}
-            <img
-              src={acwordmark}
-              alt="Aiden Cherniske"
-              className="h-10 w-auto dark:invert mx-auto transition-all duration-200"
-            />
-          </div>
-
           <p className="text-sm text-text/70">
-            © {currentYear}. All rights reserved.
+            Aiden Cherniske © {currentYear}. All rights reserved.
           </p>
         </div>
       </div>
